06c8adad782ea6bdf00f16bfb69aeb8dcb5e1ec8
Josh Hulbert <joshuahulbertnz@gmail.com>
2022-03-13 20:58:56 +1300

    Completed exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let characterArray = stringToReverse.split('');
+    let reversedCharacterArray = characterArray.reverse();
+    return reversedCharacterArray.join('');
 };
 
 // Do not edit below this line
