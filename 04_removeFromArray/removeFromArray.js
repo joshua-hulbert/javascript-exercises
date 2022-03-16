a95f325cae5fc38ed847a4c89cb70d51b1a277cd
Josh Hulbert <joshuahulbertnz@gmail.com>
2022-03-16 19:24:02 +1300

    Finished exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    for (let i = 1; i < arguments.length; i++)
+    {
+        const indexToRemove = array.indexOf(arguments[i]);
+        if (indexToRemove >= 0) {
+            array.splice(indexToRemove, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
