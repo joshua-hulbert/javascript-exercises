6edf768cf860e00609a420ba03802591c5d51f53
Josh Hulbert <joshuahulbertnz@gmail.com>
2022-03-16 19:46:31 +1300

    done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let min, max, sum;
 
+    if (x < 0 || y < 0 || !Number.isInteger(x) || !Number.isInteger(y)) {
+        return 'ERROR';
+    }
+
+    if (x < y) {
+        min = x;
+        max = y;
+    } else {
+        min = y;
+        max = x;
+    }
+
+    sum = max;
+
+    for (; min < max; min++) {
+        sum += min;
+    }
+    return sum;
 };
 
 // Do not edit below this line
